28872bf8180e750eeec798d58a93fdd43bc85d0a
Luis Fernando <xvleg3ndaryxv@gmail.com>
2017-11-25 22:02:21 -0600

    Initial Commit

---
+++ TableCellPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableCellPractice
+//
+//  Created by Luis Santiago  on 11/25/17.
+//  Copyright © 2017 Luis Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
