4df9a0bb0d5f2f991b5361f2742bc819a0868acd
Luis Fernando <xvleg3ndaryxv@gmail.com>
2017-11-28 08:36:17 -0600

    third tutorial done

--- TableCellPractice/AppDelegate.swift
+++ TableCellPractice/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds);
+        let flowLayout = UICollectionViewFlowLayout();
+        let customCollectionViewController = CustomCollectionViewController(collectionViewLayout: flowLayout);
+        let navegationController = UINavigationController(rootViewController: customCollectionViewController);
+        window?.rootViewController = navegationController;
+        window?.makeKeyAndVisible();
         return true
     }
 
--- TableCellPractice/ViewController.swift
+++ TableCellPractice/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CustomCollectionViewController : UICollectionViewController  , UICollectionViewDelegateFlowLayout{
 
+    let cellId = "cellId";
+    let names = ["Mark", "Steve" , "Carl" , "Luis", "Elon musk"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "My tableview";
+        navigationController?.navigationBar.prefersLargeTitles = true;
+        collectionView?.backgroundColor = .white;
+        collectionView?.register(CustomCell.self, forCellWithReuseIdentifier: cellId);
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return names.count;
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CustomCell;
+        cell.customLabel.text = names[indexPath.item];
+        return cell;
+    }
+ 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200);
+    }
+}
 
-
+class CustomCell : UICollectionViewCell{
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+        setUpViews();
+    }
+    
+    let customLabel:UILabel = {
+        let label = UILabel();
+        label.text = "Custom label";
+        label.translatesAutoresizingMaskIntoConstraints = false;
+        return label;
+    }();
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpViews(){
+        backgroundColor = .blue;
+        addSubview(customLabel);
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":customLabel]));
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":customLabel]));
+    }
 }
 
